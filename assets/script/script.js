b6ced76a73356407295858bb30486da71f7ef8b6
Kevyn Lima <kevynlimasilva1@gmail.com>
2024-11-05 17:16:23 -0300

    Separando o consumo da Api da manipulação de HTML'

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,4 @@
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 function convertPokemonToLi(pokemon){
     return `
@@ -24,16 +21,13 @@ function convertPokemonToLi(pokemon){
 const pokemonList = document.getElementById('pokemonList');
 
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
+pokeApi.getPokemons().then((pokemons) => {
+    const listItems = []
 
-        for(let i = 0; i < pokemons.length; i++){
-            const pokemon = pokemons[i];
-            console.log(convertPokemonToLi(pokemon))
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-            }
-            
-        })
-    .catch((error) =>console.log(error))
+    for(let i = 0; i < pokemons.length; i++){
+        const pokemon = pokemons[i];
+        listItems.push(convertPokemonToLi(pokemon))
+    }
+
+    console.log(listItems)
+})
